bef6390909ae3222973a2e922684f9d12c9e9e26
koreaaerospaceU <zx811@naver.com>
2021-10-12 20:29:22 +0900

    fix(service): 전진 조건 값 변경

--- src/main/java/racinggame/service/RacingService.java
+++ src/main/java/racinggame/service/RacingService.java
@@ -6,7 +6,7 @@ import nextstep.utils.Randoms;
  * Created by Minky on 2021-10-12
  */
 public class RacingService {
-    public static final int START_PICK_NUMBER = 1;
+    public static final int START_PICK_NUMBER = 0;
     public static final int END_PICK_NUMBER = 9;
 
     public int getRandomValue() {
