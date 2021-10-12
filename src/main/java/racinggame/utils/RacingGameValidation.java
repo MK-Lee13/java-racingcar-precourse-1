3c8bede7a64911c5f12574596dab50a1f3e85a3e
koreaaerospaceU <zx811@naver.com>
2021-10-12 15:23:41 +0900

    refactor(utils): Pattern 객체 정규 표현식 문자열 상수화

--- src/main/java/racinggame/utils/RacingGameValidation.java
+++ src/main/java/racinggame/utils/RacingGameValidation.java
@@ -6,7 +6,8 @@ import java.util.regex.Pattern;
  * Created by Minky on 2021-10-12
  */
 public class RacingGameValidation {
-    private static final Pattern pattern = Pattern.compile("[1-9]+[0-9]*");
+    public static final String POSITIVE_NUMBER_REGEX = "[1-9]+[0-9]*";
+    private static final Pattern pattern = Pattern.compile(POSITIVE_NUMBER_REGEX);
 
     public static boolean isPositiveNumber(String input) {
         return pattern.matcher(input).matches();
