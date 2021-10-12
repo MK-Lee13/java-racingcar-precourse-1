ec9a8cacf08a8cdcb595bfcb1e6fda060650c8f6
koreaaerospaceU <zx811@naver.com>
2021-10-13 00:50:14 +0900

    refactor(error): Message 필드 생성 - Error Message

--- src/main/java/racinggame/domain/ErrorStatus.java
+++ src/main/java/racinggame/domain/ErrorStatus.java
@@ -4,7 +4,21 @@ package racinggame.domain;
  * Created by Minky on 2021-10-12
  */
 public enum ErrorStatus {
-    IS_NULL_OR_EMPTY_STRING, IS_MORE_THAN_FIVE, CORRECT, IS_DUPLICATE, IS_NOT_POSITIVE_NUMBER;
+    IS_NULL_OR_EMPTY_STRING("[ERROR] 빈 문자열은 사용이 불가합니다."),
+    IS_MORE_THAN_FIVE("[ERROR] 차 이름의 길이는 5이하로 지정하세요."),
+    CORRECT(""),
+    IS_DUPLICATE("[ERROR] 차 이름은 중복될 수 없습니다."),
+    IS_NOT_POSITIVE_NUMBER("[ERROR] [1 - 9] 사이의 양수만 입력하세요('01' 같은 형식 제외).");
+
+    private final String value;
+
+    ErrorStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 
     public boolean isCorrect() {
         return this == CORRECT;
