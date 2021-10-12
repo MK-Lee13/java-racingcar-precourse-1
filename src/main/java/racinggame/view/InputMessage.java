dda2b8a7c76a21eb86e4220144bf3ce4e9a60de3
koreaaerospaceU <zx811@naver.com>
2021-10-13 00:49:36 +0900

    feat(message): Message Constant 클래스 생성 - Input Message - Report Message

---
+++ src/main/java/racinggame/view/InputMessage.java
@@ -0,0 +1,11 @@
+package racinggame.view;
+
+/**
+ * Created by Minky on 2021-10-13
+ */
+public class InputMessage {
+    private InputMessage() { }
+
+    public static String CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)";
+    public static String NUMERIC_MESSAGE = "시도할 횟수는 몇회인가요?";
+}
---
+++ src/main/java/racinggame/view/ReportMessage.java
@@ -0,0 +1,12 @@
+package racinggame.view;
+
+/**
+ * Created by Minky on 2021-10-13
+ */
+public class ReportMessage {
+    private ReportMessage() { }
+
+    public static String WINNERS_MESSAGE = "최종 우승자는 %s 입니다.";
+    public static String START_MESSAGE = "실행 결과";
+    public static String INTERIM_MESSAGE = "%s : %s";
+}
